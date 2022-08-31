3bbbeaa4a8e22a6cfa079030db70bbdec4e16a56
현 HYEON <96094433+HYEON0425@users.noreply.github.com>
2022-08-31 19:44:08 +0900

    day3

--- MainListView.swift
+++ MainListView.swift
@@ -12,8 +12,16 @@ struct MainListView: View {
     
     var body: some View {
         List(store.list) { memo in
-            Text(memo.content)
+            VStack(alignment: .leading) {
+                Text(memo.content)
+                    .font(.body)
+                    .lineLimit(1)
+                Text(memo.insertDate, style: .date)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
         }
+        .listStyle(.plain)
         .navigationTitle("내 메모")
     }
 }
